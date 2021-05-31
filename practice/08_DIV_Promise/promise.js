fe798872726a044117a42cf30439e3bbc1b16ac3
ZihaoHan <972802149@qq.com>
2021-05-30 20:42:59 -0700

    state only change once

--- practice/08_DIV_Promise/promise.js
+++ practice/08_DIV_Promise/promise.js
@@ -3,11 +3,13 @@ function Promise(executor){
     this.promiseResult = null
     self = this
     function resolve(data){
+        if(self.promiseState !== 'pending') return
         self.promiseState = 'fullfilled'
         self.promiseResult = data
     }
 
     function reject(data){
+        if(self.promiseState !== 'pending') return
         self.promiseState = 'rejected'
         self.promiseResult = data
     }
